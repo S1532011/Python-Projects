84163e5db4362802d5d2d3ea5b8a32ce5893ca66
S1532011 <S1532011@student.mcckc.edu>
2023-02-14 15:32:49 -0600

    data input

--- D&D App/Character.py
+++ D&D App/Character.py
@@ -1,11 +1,14 @@
 import json
 
 class Character:
-    def getData(self, characterFile):
+    character = {}
+
+    def getData(self, character):
+        characterFile = "players/" + character + ".json"
         file = open(characterFile, "r")
         character = json.dumps(file.read())
         file.close()
         return character
     
-    def __init__(self, characterFile):
-        self.getData(characterFile)
+    def __init__(self, character):
+        self.character = self.getData(character)
